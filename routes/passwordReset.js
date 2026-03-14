cad0f8b1974e752151d7cbe0c4938faf65ac4ed4
Muoyheangchhea <muoyheang12345@gmail.com>
2026-03-15 04:17:40 +0800

    Switch from bcrypt to bcryptjs for Railway compatibility

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../db');
 const nodemailer = require('nodemailer');
 
